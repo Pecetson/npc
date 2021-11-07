6c908c79bb29a64ba23576a12f5a4e291dc3c42c
Pecetson <ryuusakimahirojz@gmail.com>
2021-11-07 20:57:55 +0100

    Some fixes

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,25 @@
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] }); //new Client instance
-
+// new Client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// Applies only once when the bot is ready.
 client.once('ready', () => {
-	console.log('Pecet logged in!'); //Applies only once when the bot is ready.
+	console.log('Pecet logged in!');
 });
-
-client.on('interactionCreate', async interaction => { //on function goes async and arrow.
+// on function goes async and arrow.
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
 
 	if (commandName === 'ping') {
 		await interaction.reply(`Pong!\nMy ping: ${Date.now() - interaction.createdTimestamp}`);
-	} else if (commandName === 'server') {
+	}
+	else if (commandName === 'server') {
 		await interaction.reply(`Server: ${interaction.guild.name}\nMember Count: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
+	}
+	else if (commandName === 'user') {
 		await interaction.reply('');
 	}
 });
